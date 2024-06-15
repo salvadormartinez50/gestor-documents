386fca1954e4fcec8f7de9998d32c5c2b9c54dff
iccedevelop-admin <cchhava@gmail.com>
2024-06-15 16:18:18 -0600

    Add lombok annotation

--- src/main/java/org/documents/dtos/Message.java
+++ src/main/java/org/documents/dtos/Message.java
@@ -1,83 +1,12 @@
 package org.documents.dtos;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Message {
     private String code;
     private String messageInCode;
 
-    public String getCode() {
-        return this.code;
-    }
-
-    public String getMessageInCode() {
-        return this.messageInCode;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public void setMessageInCode(String messaje) {
-        this.messageInCode = messaje;
-    }
-
-    public boolean equals(Object o) {
-        if (o == this) {
-            return true;
-        } else if (!(o instanceof Message)) {
-            return false;
-        } else {
-            Message other = (Message)o;
-            if (!other.canEqual(this)) {
-                return false;
-            } else {
-                Object thisCode = this.getCode();
-                Object otherCode = other.getCode();
-                if (thisCode == null) {
-                    if (otherCode != null) {
-                        return false;
-                    }
-                } else if (!thisCode.equals(otherCode)) {
-                    return false;
-                }
-
-                Object thisMessage = this.getMessageInCode();
-                Object otherMessage = other.getMessageInCode();
-                if (thisMessage == null) {
-                    if (otherMessage != null) {
-                        return false;
-                    }
-                } else if (!thisMessage.equals(otherMessage)) {
-                    return false;
-                }
-
-                return true;
-            }
-        }
-    }
-
-    protected boolean canEqual(Object other) {
-        return other instanceof Message;
-    }
-
-    public int hashCode() {
-        int result = 1;
-        Object code1 = this.getCode();
-        result = result * 59 + (code1 == null ? 43 : code1.hashCode());
-        Object message1 = this.getMessageInCode();
-        result = result * 59 + (message1 == null ? 43 : message1.hashCode());
-        return result;
-    }
-
-    public String toString() {
-        String var10000 = this.getCode();
-        return "Mensaje(codigo=" + var10000 + ", mensaje=" + this.getMessageInCode() + ")";
-    }
-
-    public Message(String code, String message) {
-        this.code = code;
-        this.messageInCode = message;
-    }
-
-    public Message() {
-    }
 }
